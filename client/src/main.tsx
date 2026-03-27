350dc36f8e656c06a69a76fedf83998d320de8ae
selenai9 <s.isimbi2@alustudent.com>
2026-03-27 22:20:47 +0200

    Add Inter font styles to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,6 @@
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
 import { trpc } from "@/lib/trpc";
 import { UNAUTHED_ERR_MSG } from '@shared/const';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
